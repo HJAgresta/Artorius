f40f2ba7e3b48c3e59b261d423c13a5972a7ec51
HJAgresta <hjagresta@gmail.com>
2018-12-13 20:47:28 -0500

    Initial Commit
    Well cant rever less than this

---
+++ Assets/Characters/Character.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+	
+	public Vector2 acc = new Vector2(0.0f,0.0f);
+	public Vector2 vel = new Vector2(0.0f,0.0f);
+	public bool grounded = false;
+	public float fallAcc;
+
+	public float grdMve = 0.5f;
+	public float airMve = 0.1f;
+	public float slippery = 0.5f;
+	public float jumpSpeed = 2f;
+
+	protected void Jump(float delay, float upVelocity)
+	{
+		acc.y = acc.y + upVelocity;
+	}
+
+	public virtual void Update()
+	{
+
+		if (!grounded) 
+		{
+			acc.y = acc.y - fallAcc;
+		}
+	}
+
+	/// <summary>
+	/// Stops the character.
+	/// </summary>
+	/// <param name="dir">Direction to stop character in. 0=X,1=Y,2=X&Y</param>
+	protected void StopCharacter(int dir){
+		if (dir == 0) 
+		{
+			vel.x = 0.0f;
+		} 
+		else if (dir == 1) 
+		{
+			vel.y = 0.0f;
+		} 
+		else if (dir == 2) 
+		{
+			vel.Set (0.0f, 0.0f);
+		}
+	}
+}
---
+++ Assets/Characters/Player/PlayerControls.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : Character 
+{
+	// Update is called once per frame
+	public override void Update () 
+	{
+		base.Update ();
+
+		if (Input.GetKey ("w")) 
+		{
+			if (grounded) 
+			{
+				Jump (0.0f, 5);
+			}
+		}
+
+
+		if (Input.GetKey ("s")) 
+		{
+		}
+
+
+		if (Input.GetKey ("a")) 
+		{
+			if (grounded) 
+			{
+				acc.x = acc.x - grdMve;
+			} 
+			else 
+			{
+				acc.x = acc.x - airMve;
+			}
+		}
+
+		if (Input.GetKey ("d")) 
+		{
+			if (grounded) 
+			{
+				acc.x = acc.x + grdMve;
+			} 
+			else 
+			{
+				acc.x = acc.x + airMve;
+			}
+		}
+
+
+		vel = vel + acc;
+
+		gameObject.transform.Translate (vel);
+
+		vel.x = vel.x * slippery;
+
+		if (vel.x < 0.1f) 
+		{
+			StopCharacter (0);
+		}
+
+		if (vel.y < 0.1f) 
+		{
+			StopCharacter (1);
+		}
+		acc = new Vector2();
+	}
+}
